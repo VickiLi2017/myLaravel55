f2f1885f88ace74f753099e52df66726c6438bfb
VickiLi2017 <vl54@njit.edu>
2017-09-29 14:19:06 -0400

    Add index.blade.php to display list of products in database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all()->toArray();
+        return view('products.index', compact('products'));
+
     }
 
     /**
